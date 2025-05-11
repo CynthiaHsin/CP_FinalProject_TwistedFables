df7e3f23fd5b5f1d67e4754bec6af523ee291732
CynthiaHsin <20051025cynthia@gmail.com>
2025-05-12 01:46:41 +0800

    main define update

--- main.h
+++ main.h
@@ -5,5 +5,7 @@
 # include <stdlib.h>
 # include <string.h>
 # include <time.h>
+# include <math.h>
+# include <sys/param.h>
 # include "main_debug.h"
 # include "main_config.h"
--- main_config.h
+++ main_config.h
@@ -1,8 +1,5 @@
 # pragma once
 
-# include <stdio.h>
-# include <stdint.h>
-
 # define CARD_TYPE_NAME_MAX 60
 typedef enum _eCardType {
     CARD_ORIGINAL = -1,
@@ -55,6 +52,7 @@ typedef enum _eCardType {
     CARD_TYPE_NUM                // 標記不同卡的數量 
 } eCardType;
 
+# define CARD_COST_ORIGINAL -1
 
 # define CARD_SPACE_NAME_MAX 30
 typedef enum _eCardSpace{
@@ -67,10 +65,11 @@ typedef enum _eCardSpace{
     CARD_SPACE_DECK_POISON, // 牌庫：毒
     CARD_SPACE_THROW,       // 棄牌堆
     CARD_SPACE_USE,         // 出牌堆
+    CARD_SPACE_USE_LASTING, // 持續效果的牌
     CARD_SPACE_SHOP,        // 購牌區
     CARD_SPACE_DELETE,      // 移除
     CARD_SPACE_NUM
-    
+
 } eCardSpace;
 
 typedef enum _eCharacter {
