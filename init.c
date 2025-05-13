eb393440f1f8563cd01352e70df5d4583fa872c4
WillyLin <linliqi20031118@gmail.com>
2025-05-14 01:59:54 +0800

    choose character

--- init.c
+++ init.c
@@ -3,9 +3,12 @@
 #include <SDL2/SDL_timer.h> 
 #include <SDL2/SDL_ttf.h>
 #include "main.h"
+#include <stdbool.h>
 
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
+#define IMG_SIZE 150
+#define SCALE 1.2
 
 int main(){
     // retutns zero on success else non-zero 
@@ -42,6 +45,15 @@ int main(){
         return 1;
     }
 
+    SDL_Texture* character[6] = { // load charcter image for choose
+        IMG_LoadTexture(ren, "assets/alice.jpg"),
+        IMG_LoadTexture(ren, "assets/kaguya.jpg"),
+        IMG_LoadTexture(ren, "assets/mulan.jpg"),
+        IMG_LoadTexture(ren, "assets/red_riding_hood.jpg"),
+        IMG_LoadTexture(ren, "assets/sleeping_beauty.jpg"),
+        IMG_LoadTexture(ren, "assets/snow_white.jpg")
+    };
+
     // get the size of the image
     int32_t texW, texH;
     SDL_QueryTexture(image, NULL, NULL, &texW, &texH);
@@ -69,6 +81,57 @@ int main(){
         SDL_RenderPresent(ren);
         SDL_Delay(10); // control the fade out speed
     }
+
+
+    // choose the character
+    int32_t selected = 0;
+    bool running = true;
+    SDL_Event e;
+
+    while (running) {
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT)
+                running = false;
+            else if (e.type == SDL_KEYDOWN) {
+                switch (e.key.keysym.sym) {
+                    case SDLK_LEFT:  if (selected % 3 > 0) selected--; break;
+                    case SDLK_RIGHT: if (selected % 3 < 2) selected++; break;
+                    case SDLK_UP:    if (selected >= 3) selected -= 3; break;
+                    case SDLK_DOWN:  if (selected < 3) selected += 3; break;
+                    case SDLK_RETURN:
+                        printf("You selected image #%d\n", selected + 1);
+                        running = false;
+                        break;
+                }
+            }
+        }
+
+        SDL_SetRenderDrawColor(ren, 0, 0, 0, 255);
+        SDL_RenderClear(ren);
+
+        for (int i = 0; i < 6; i++) {
+            int row = i / 3, col = i % 3;
+            SDL_Rect dst = {
+                150 + col * (IMG_SIZE + 40),
+                100 + row * (IMG_SIZE + 40),
+                IMG_SIZE,
+                IMG_SIZE
+            };
+
+            if (i == selected) {
+                int scale = IMG_SIZE * SCALE;
+                dst.x -= (scale - IMG_SIZE) / 2;
+                dst.y -= (scale - IMG_SIZE) / 2;
+                dst.w = dst.h = scale;
+            }
+
+            SDL_RenderCopy(ren, character[i], NULL, &dst);
+        }
+
+        SDL_RenderPresent(ren);
+        SDL_Delay(16);
+    }
+
 
     SDL_DestroyTexture(image);
     SDL_DestroyRenderer(ren);
