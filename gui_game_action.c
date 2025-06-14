4560e9fcabefb2dc6677cae4e2f298120eaa4261
CynthiaHsin <20051025cynthia@gmail.com>
2025-06-15 06:29:55 +0800

    .

--- gui_game_action.c
+++ gui_game_action.c
@@ -20,7 +20,7 @@ int32_t gui_action_choose_card (int32_t *pCard_type, sCardData cards[], int32_t 
     // }
 }
 
-int32_t gui_action_choose_token (int32_t token_use_max, int32_t player){
+int32_t gui_action_choose_token (int32_t token_use_max, int32_t player, char * text){
     
 }
 
